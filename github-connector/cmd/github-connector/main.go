8e7809079b92d7c53cb1ba6492afb74c6bf5fd9c
pPrecel <filip.strozik@outlook.com>
2019-07-23 12:51:58 +0200

    copy all files from another repository

--- github-connector/cmd/github-connector/main.go
+++ github-connector/cmd/github-connector/main.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 	"os"
 
+	tmpfunc "github.com/kyma-incubator/hack-showcase/github-connector/internal"
 	"github.com/kyma-incubator/hack-showcase/github-connector/internal/githubwrappers"
 	"github.com/kyma-incubator/hack-showcase/github-connector/internal/handlers"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+
+	tmpfunc.TmpFunc()
 
 	log.Println("server started")
 	webhook := handlers.NewWebHookHandler(
