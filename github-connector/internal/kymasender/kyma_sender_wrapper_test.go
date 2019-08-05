43160bdea26d279161790dc98050e37731389373
Karol Jaksik <jaksikkarol@gmail.com>
2019-08-05 08:41:34 +0200

    Add more tests

--- github-connector/internal/kymasender/kyma_sender_wrapper_test.go
+++ github-connector/internal/kymasender/kyma_sender_wrapper_test.go
@@ -35,8 +35,8 @@ func TestSendToKyma_ProperArguments(t *testing.T) {
 	})
 }
 
-func TestWebhookHandler_sendToKyma(t *testing.T) {
-	t.Run("should return no error", func(t *testing.T) {
+func TestSendToKyma_NoEventType(t *testing.T) {
+	t.Run("should return an internal error", func(t *testing.T) {
 		payload := toJSON{TestJSON: "test"}
 		toSend, err := json.Marshal(payload)
 		require.NoError(t, err)
@@ -45,6 +45,34 @@ func TestWebhookHandler_sendToKyma(t *testing.T) {
 		k := NewKymaSenderWrapper(&ClientMock{}, mockParser)
 		expected := apperrors.Internal("test")
 		actual := k.SendToKyma("", "v1", "", "github-connector-app", json.RawMessage(toSend))
+		assert.Equal(t, expected.Code(), actual.Code())
+	})
+}
+
+func TestSendToKyma_NoEventVersion(t *testing.T) {
+	t.Run("should return an internal error", func(t *testing.T) {
+		payload := toJSON{TestJSON: "test"}
+		toSend, err := json.Marshal(payload)
+		require.NoError(t, err)
+		mockParser := &mocks.EventParser{}
+		mockParser.On("GetEventRequestPayload", "issuesevent.opened", "", "", "github-connector-app", json.RawMessage(toSend)).Return(eventparser.EventRequestPayload{}, apperrors.Internal("test"))
+		k := NewKymaSenderWrapper(&ClientMock{}, mockParser)
+		expected := apperrors.Internal("test")
+		actual := k.SendToKyma("issuesevent.opened", "", "", "github-connector-app", json.RawMessage(toSend))
+		assert.Equal(t, expected.Code(), actual.Code())
+	})
+}
+
+func TestSendToKyma_NoSourceID(t *testing.T) {
+	t.Run("should return an internal error", func(t *testing.T) {
+		payload := toJSON{TestJSON: "test"}
+		toSend, err := json.Marshal(payload)
+		require.NoError(t, err)
+		mockParser := &mocks.EventParser{}
+		mockParser.On("GetEventRequestPayload", "issuesevent.opened", "v1", "", "", json.RawMessage(toSend)).Return(eventparser.EventRequestPayload{}, apperrors.Internal("test"))
+		k := NewKymaSenderWrapper(&ClientMock{}, mockParser)
+		expected := apperrors.Internal("test")
+		actual := k.SendToKyma("issuesevent.opened", "v1", "", "", json.RawMessage(toSend))
 		assert.Equal(t, expected.Code(), actual.Code())
 	})
 }
