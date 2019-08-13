2bf1f4aa46f9fd9dabea8607012d00008c371420
Rafal Potempa <rafal.potem@gmail.com>
2019-08-13 15:32:09 +0200

    Delete tests to wait for refactor

--- slack-connector/cmd/main.go
+++ slack-connector/cmd/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"net/http"
-	"os"
-
+	"github.com/kyma-incubator/hack-showcase/slack-connector/internal/registration"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	log.Info("server started")
 
-	id, err := registration.RegisterService()
+	builder := registration.NewServiceDetailsBuilder()
+	requestSender := registration.NewRegisterRequestSender()
+	service := registration.NewServiceRegister(builder, requestSender)
+	id, err := service.RegisterService()
 	if err != nil {
 		log.Fatal("Fatal error: ", err.Error())
 	}
-	log.WithFields(log.Fields{"id": id,}).Info("Service registered")
+	log.WithFields(log.Fields{
+		"id": id,
+	}).Info("Service registered")
 }
